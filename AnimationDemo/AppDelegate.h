1eab71d161f757e393c9f7363c0d1256c6e25442
waveboyz <308086427@qq.com>
2016-05-30 10:28:42 +0800

    Initial Commit

---
+++ AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by 周翔 on 16/5/30.
+//  Copyright © 2016年 周翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by 周翔 on 16/5/30.
+//  Copyright © 2016年 周翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
